ce1fb52f33f5434328aa348dcc18abd1cfb79a65
Jeevittan <77376456+Jeevittan@users.noreply.github.com>
2022-01-16 16:55:04 +0800

    Final Commit (Unsuccesfull input detection)

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
-var timer = document.getElementsByClassName("timer");
-var score = document.getElementsByClassName("score");
-var initTime = 20;
+var updateTimer = document.querySelector(".timer");
+var words = document.querySelector(".word");
+var initTimer = document.querySelector(".time");
+var initScore = document.querySelector(".score");
+var button = document.querySelector("button");
+var score = 0;
+var time = 20;
+var typed;
 
+// Play music when start button is clicked
+document.querySelector("button").addEventListener("click", function (e) {
+  var gameMusic = new Audio("musics/test2.mp3");
+  countdown();
+  gameMusic.play();
+  selectRandomWord(e);
+  button.disabled = true;
+});
 
-button.eventL
-// READ words.txt
-// var fs = require('fs');
-// var file = fs.readFileSync("words.txt", 'utf-8');
-// var word = text.split('\n');
-//array
-const words = [];
-words.push("word");
-document.getElementById("word").innerHTML = words;
-
-// GENERATE RANDOM WORDS FROM words.txt
-let myWords = words[Math.floor(Math.random() * words.length)]
-
-
-//pseudocode: read textfile, array, generate random words, start button, disable button until timer stop
-console.log(myWords)//This gives you any string from groceries 
-
-//add score, kalau betul add sound, next
- 
-
-function startType{
-    console.log(myWords)//This gives you any string from groceries
+// Function to start the game timer 20seconds
+function countdown() {
+  score = 0;
+  var timer = setInterval(function () {
+    button.disabled = true;
+    time--;
+    updateTimer.innerHTML = time;
+    if (time === 0) {
+      alert("Game over! Your score is " + score);
+      initScore.innerHTML = "0";
+      words.innerHTML = "";
+      button.disabled = false;
+      clearInterval(timer);
+      time = 20;
+      initTimer.innerHTML = "20";
+      button.disabled = false;
+    }
+  }, 1000);
 }
 
+//List of words
+const wordsList = [
+  "mountain",
+  "ninth",
+  "ocean",
+  "office",
+  "parent",
+  "peanut",
+  "pencil",
+  "picnic",
+  "police",
+  "pretty",
+  "across",
+  "against",
+  "answer",
+  "between",
+  "board",
+];
 
+// Function to select random word from the list
+function selectRandomWord(e) {
+  // words.innerHTML = "";
+  var random = Math.floor(Math.random() * 15);
+  words.innerHTML = wordsList[random];
+}
+
+// function checkTypedWord(currWord) {
+//     var x = document.getElementByClass("input").value;
+//     typed.push(x);
+// }
